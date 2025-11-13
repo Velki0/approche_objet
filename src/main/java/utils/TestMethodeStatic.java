e0db834e97d02dbb02b8b9da2a34dd297a27b4ea
Velki0 <b.gerardin125@gmail.com>
2025-11-13 14:59:05 +0100

    Réalisation TP04

---
+++ src/main/java/utils/TestMethodeStatic.java
@@ -0,0 +1,17 @@
+package utils;
+
+public class TestMethodeStatic {
+
+    public static void main(String[] args) {
+
+        String chaine = "12";
+        int chaineConvertiInt = Integer.parseInt(chaine);
+        System.out.println(chaineConvertiInt);
+
+        int a = 500;
+        int b = 3;
+        int maximumEntreAetB = Integer.max(a, b);
+        System.out.println(maximumEntreAetB);
+
+    }
+}
